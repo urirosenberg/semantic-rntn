3d5c5f31fc63f0fb3372a214ed8043e57611f770
urirosenberg <urirosenberg75@gmail.com>
2015-07-07 14:30:04 +0300

    initial setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+__author__ = 'Uri Rosenberg'
+from setuptools import setup
+
+setup(name='semantic_rntn',
+      version='0.1',
+      description='Recursive Neural Tensor Network (And Recursive NN) for sentiment classification',
+      url='https://github.com/urirosenberg/semantic-rntn',
+      author='Uri Rosenbergs',
+      packages=['semantic_rntn'],
+      zip_safe=False)
